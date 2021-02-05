9a9fa222c3356f23f9c05e1b38e6e067d5238838
rBpJHUAR <tyvoitenko@gmail.com>
2021-02-05 16:28:54 +0200

    Added deleting products

--- src/main/java/servlets/products/DeleteProductServlet.java
+++ src/main/java/servlets/products/DeleteProductServlet.java
@@ -1,4 +1,39 @@
 package servlets.products;
 
-public class DeleteProductServlet {
+import dao.product.MysqlProductDaoImpl;
+import org.apache.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/products/delete")
+public class DeleteProductServlet extends HttpServlet {
+    private MysqlProductDaoImpl productDao;
+    private static final Logger LOGGER = Logger.getLogger(DeleteProductServlet.class);
+
+    {
+        productDao = new MysqlProductDaoImpl();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOGGER.info("Processing get request");
+
+        try {
+            int productId = Integer.parseInt(request.getParameter("id"));
+            productDao.deleteProduct(productId);
+        } catch (Exception e) {
+            LOGGER.info(e.getStackTrace());
+            e.printStackTrace();
+
+            RequestDispatcher req = request.getRequestDispatcher("/views/products/add/failedToCreateProduct.jsp");
+            req.include(request, response);
+        }
+
+        response.sendRedirect("/products");
+    }
 }
